b7432257e6998294a1923375e73c8c127f4fa098
Gohar Simonyan <Gohar.Simonyan@contracted.pmi.com>
2025-07-15 20:03:56 +0400

    add parameter types

--- ui-tests/cucumber/features/launches.feature
+++ ui-tests/cucumber/features/launches.feature
@@ -7,8 +7,8 @@ Feature: Launches
 
   Scenario: As a user I should be able to remove launch
     When I get "Launch Item Names" element text and save as "launchName"
-      And I click on "first" "Hamburger Menu" element
-      And I click on "second" "Delete Option" element
+      And I click on first "Hamburger Menu" element
+      And I click on second "Delete Option" element
       And I click on "Delete Submit Button" element
       And I reload "Launches" page
     Then "Launch Item Names" collection should not include "launchName" member
--- ui-tests/cucumber/steps/steps.js
+++ ui-tests/cucumber/steps/steps.js
@@ -2,27 +2,24 @@ import {Given, When, Then} from '@cucumber/cucumber';
 import {expect} from '@playwright/test';
 import * as testData from '../test-data/testData.js';
 import { PageFactory } from '../page-object/pageFactory.js';
-import { camelCaseTransformer } from '../utils/utils.js';
-import { wordsToNumbers } from 'words-to-numbers';
 
 Given('I navigate to {string} page', async function (pageName) {
     this.currentPage = PageFactory.getPageInstance(pageName, this.page);
     await this.currentPage.openPage();
 });
 
-When('I get {string} element text and save as {string}', async function (element, key) {
-    const elementLocator = this.currentPage[camelCaseTransformer(element)];
+When('I get {element} text and save as {string}', async function (element, key) {
+    const elementLocator = this.currentPage[element];
     this[key] = await elementLocator.first().textContent();
 });
 
-When('I click on {string} {string} element', async function (index, element) {
-    const resolvedIndex = wordsToNumbers(index) - 1;
-    const locator = this.currentPage[camelCaseTransformer(element)].nth(resolvedIndex);
+When('I click on {index} {element}', async function (index, element) {
+    const locator = this.currentPage[element].nth(index);
     await locator.click();
 });
 
-When('I click on {string} element', async function (element) {
-    const locator = this.currentPage[camelCaseTransformer(element)];
+When('I click on {element}', async function (element) {
+    const locator = this.currentPage[element];
     await locator.click();
 });
 
@@ -31,12 +28,12 @@ When('I reload {string} page', async function (pageName) {
     await this.currentPage.reload();
 });
 
-Then('{string} collection should equal {string} data', async function (collection, data) {
-    await expect(this.currentPage[camelCaseTransformer(collection)]).toHaveText(testData[data], { useInnerText: true });
+Then('{collection} should equal {string} data', async function (collection, data) {
+    await expect(this.currentPage[collection]).toHaveText(testData[data], { useInnerText: true });
 });
 
-Then('{string} collection should not include {string} member', async function (collection, member) {
-    const collectionLocator = this.currentPage[camelCaseTransformer(collection)];
+Then('{collection} should not include {string} member', async function (collection, member) {
+    const collectionLocator = this.currentPage[collection];
     const filteredLocator = collectionLocator.filter({ hasText: this[member] });
     await expect(filteredLocator).toHaveCount(0);
 });
