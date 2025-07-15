ad123b1630ce5c3f75f7f5e039ba2728260f98b0
Gohar Simonyan <Gohar.Simonyan@contracted.pmi.com>
2025-07-15 11:40:18 +0400

    add custom world

--- ui-tests/cucumber/support/hooks.js
+++ ui-tests/cucumber/support/hooks.js
@@ -3,13 +3,10 @@ import { chromium } from 'playwright';
 import * as fs from 'fs';
 import path from 'path';
 import { LoginPage } from '../page-object/loginPage.js';
-import {PageFactory} from '../page-object/pageFactory.js';
+import { PageFactory } from '../page-object/pageFactory.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
-let browser;
-let context;
 
 const STORAGE_STATE_PATH = path.resolve('./storageState.json');
 
@@ -19,28 +16,29 @@ Before(async function () {
         username: process.env.USER_NAME_0,
         password: process.env.PASSWORD_0,
     };
-    browser = await chromium.launch({ headless: false });
-
+    this.browser = await chromium.launch({ headless: false });
     if (fs.existsSync(STORAGE_STATE_PATH)) {
-        context = await browser.newContext({
+        this.context = await this.browser.newContext({
             storageState: STORAGE_STATE_PATH,
             baseURL: baseUrl,
         });
     } else {
-        context = await browser.newContext({ baseURL: baseUrl });
-        const page = await context.newPage();
+        this.context = await this.browser.newContext({ baseURL: baseUrl });
+        const page = await this.context.newPage();
         const loginPage = new LoginPage(page);
         await loginPage.openPage();
         await loginPage.login(credentials);
         await page.waitForLoadState('networkidle');
-        await context.storageState({ path: STORAGE_STATE_PATH });
+        await this.context.storageState({ path: STORAGE_STATE_PATH });
         await page.close();
     }
-    this.context = context;
-    this.page = await context.newPage();
+    this.page = await this.context.newPage();
     this.currentPage = PageFactory.getPageInstance('Launches', this.page);
 });
 
 After(async function () {
-    if (browser) { await browser.close();}
+    if (this.browser) { await this.browser.close();}
+    this.context = null;
+    this.page = null;
+    this.currentPage = null;
 });
