ff33ecac8e61feb4fba8913b08c2a63d5af8426b
Gohar Simonyan <Gohar.Simonyan@contracted.pmi.com>
2025-07-18 11:10:13 +0400

    remove unused test data

--- unit-test/src/tests/test_data/user_emails.js
+++
@@ -1,3 +0,0 @@
-const user_emails = 'Sincere@april.biz;Shanna@melissa.tv;Nathan@yesenia.net;Julianne.OConner@kory.org;Lucio_Hettinger@annie.ca;Karley_Dach@jasper.info;Telly.Hoeger@billy.biz;Sherwood@rosamond.me;Chaim_McDermott@dana.io;Rey.Padberg@karina.biz';
-
-module.exports = user_emails;
